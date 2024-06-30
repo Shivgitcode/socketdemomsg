06a69b27f877870317dcd10bfe097ff681109fe7
shivansh <shivneeraj2004@gmail.com>
2024-06-30 12:55:02 +0530

    done

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const server = http.createServer(app)
 
 const io = new Server(server, {
     cors: {
-        origin: "https://socketdemomsg.vercel.app",
+        origin: "https://socketdemomsg.vercel.app/",
         methods: ["GET", "POST"]
     }
 
